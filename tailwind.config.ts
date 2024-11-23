7349761e037a2b20fb452e579d4c7fe5484ff53c
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-11-24 03:38:04 +0900

    🔧 フォントの設定を追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,4 +6,24 @@ export default defineConfig({
   content: [
     "src/**/*.astro",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "'Noto Sans JP Variable'",
+          "sans-serif",
+        ],
+        display: [
+          "'Noto Sans Display Variable'",
+          "'Noto Sans JP Variable'",
+          "sans-serif",
+        ],
+        mono: [
+          "'Noto Sans Mono Variable'",
+          "'Noto Sans JP Variable'",
+          "monospace",
+        ],
+      },
+    },
+  },
 });
