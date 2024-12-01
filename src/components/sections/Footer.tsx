4ea113fed96dd53de576e4f74a28c119459b67d5
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-12-02 03:32:32 +0900

    💄 フッターを追加

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -2,8 +2,12 @@ import { defineComponent } from "~/utils/preact.ts";
 
 export const Footer = defineComponent(() => {
   return (
-    <footer>
-      <p>footer</p>
+    <footer class="bg-primary-300 text-white">
+      <div>
+        <div class="flex justify-center items-center h-16">
+          <p>&copy; 2024 新篠ゆう</p>
+        </div>
+      </div>
     </footer>
   );
 });
